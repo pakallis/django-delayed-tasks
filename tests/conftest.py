333e6e7e3ea22051fc38726120995cd35e08c460
Pavlos Kallis <pkallis@douleutaras.com>
2022-11-17 23:54:53 +0200

    Remove redis backend

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,10 +9,7 @@ def celery_config():
     return {
         "broker_connection_timeout": 10000,
         "worker_lost_wait": 100000,
-        "redis_socket_timeout": 100000,
-        "result_backend_transport_options": {"visibility_timeout": 18000},
         "broker_url": "amqp://guest:guest@rabbitmq:5672",
-        "result_backend": "redis://redis:6379/0",
     }
 
 
